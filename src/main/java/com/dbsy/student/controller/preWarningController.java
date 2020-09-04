dfb10da91e5f48be2692014f20a5d97b9bec6d64
DIDIQi <x00208161534@njit.edu.cn>
2020-09-04 21:57:01 +0800

    DIDIQI-V1.3==学业预警

--- src/main/java/com/dbsy/student/controller/preWarningController.java
+++ src/main/java/com/dbsy/student/controller/preWarningController.java
@@ -3,13 +3,16 @@ package com.dbsy.student.controller;
 import com.dbsy.student.annotation.Authority;
 import com.dbsy.student.myenum.Role;
 import com.dbsy.student.pojo.Clazz;
+import com.dbsy.student.pojo.Student;
 import com.dbsy.student.service.ClazzService;
+import com.dbsy.student.service.StudentService;
 import com.dbsy.student.util.News;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -21,34 +24,36 @@ import java.util.Map;
 @Authority({Role.Admin})
 public class preWarningController {
 
+//    @Autowired
+//    @Qualifier("clazzServiceImp")
+//    ClazzService clazzService;
+
     @Autowired
-    @Qualifier("clazzServiceImp")
-    ClazzService clazzService;
+    @Qualifier("studentServiceImp")
+    StudentService studentService;
 
     @Authority({Role.Teacher})
     @RequestMapping("")
-    public String clazz() {
+    public String preWarning() {
         return "stuInfo/preWarning";
     }
 
 
 
-    @Authority({Role.Teacher})
     @RequestMapping("/list")
     @ResponseBody
-    public Map list(Map map) {
+    public Map list(@RequestParam Map map) {
         Map m = new HashMap();
-        m.put("total", clazzService.listCount(map));
-        m.put("rows", clazzService.list(map));
+        m.put("total", studentService.listCount(map));
+        m.put("rows", studentService.list(map));
         return m;
     }
-
 
 
     @ResponseBody
     @RequestMapping("/remove/{id}")
     public Map remove(@PathVariable("id") int id) {
-        if (clazzService.delete(id) > 0) {
+        if (studentService.delete(id) > 0) {
             return News.success();
         }
         return News.fail("删除失败");
@@ -57,8 +62,7 @@ public class preWarningController {
     @ResponseBody
     @RequestMapping("/batchRemove")
     public Map batchRemove(int[] ids) {
-
-        if (clazzService.batchRemove(ids) == ids.length) {
+        if (studentService.batchRemove(ids) == ids.length) {
             return News.success();
         }
         return News.fail("删除失败");
@@ -66,46 +70,19 @@ public class preWarningController {
 
     @ResponseBody
     @RequestMapping("/insert")
-    public Map insert(Clazz clazz) {
-        if (clazzService.insert(clazz) > 0) {
+    public Map insert(Student student) {
+        if (studentService.insert(student) > 0) {
             return News.success();
         }
         return News.fail("添加失败");
     }
 
-    @ResponseBody
-    @RequestMapping("/update")
-    public Map update(Clazz clazz) {
-        if (clazzService.update(clazz) > 0) {
-            return News.success();
-        }
-        return News.fail("添加失败");
-    }
 
-    @Authority({Role.Teacher})
     @ResponseBody
     @RequestMapping("/get/{id}")
     public Map get(@PathVariable("id") int id) {
-        return News.success("成功", clazzService.get(id));
-    }
-
-    @Authority({Role.Teacher})
-    @ResponseBody
-    @RequestMapping("/getAll")
-    public Map getAll() {
-        return News.success("成功", clazzService.getAll());
-    }
-
-    @Authority({Role.Teacher})
-    @ResponseBody
-    @RequestMapping("/getClazzByMajorId/{majorId}")
-    public Map getClazzBymajorId(@PathVariable("majorId") int majorId){
-        List list = clazzService.getClazzByMajorId(majorId);
-        if (list != null) {
-            return News.success("成功",list);
-
-        }
-        return News.fail("查找失败");
+        Student student = studentService.get(id);
+        return student != null ? News.success("OK", student) : News.fail("不存在");
     }
 
 
